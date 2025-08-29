5affd49fcfeb7ebb4d6a0c218285ef3bf519e2c3
Aesha-dhaduk <aeshadhaduk65@gmail.com>
2025-08-29 10:33:33 +0530

    install cors and add index.js

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const { default: mongoose } = require("mongoose")
 const UserRoutes = require('./routes/user')
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
+const cors = require("cors");
 
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));//join fronted and backend
 app.use(cookieParser())
+app.use(cors());
 
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
 
 app.use('/user', UserRoutes)
 mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {

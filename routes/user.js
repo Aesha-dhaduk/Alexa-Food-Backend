f365753aee9bf20c1c5060c3916793b9a39bc34a
Aesha-dhaduk <aeshadhaduk65@gmail.com>
2025-08-30 17:47:35 +0530

    123

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { adduser, alluser, singleuser, updateuser, deleteuser, login } = require('../controllers/user')
 const { auth } = require('../middleware/user')
 //sdsdsd
+//sdsdsd
 const route = express.Router()
 
 

c97d92064783268b57beee195b4ced39ba80b118
Archi-Savani <aaru.jbs07@gmail.com>
2025-08-30 13:39:40 +0530

    remove cors dublicate

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));//join fronted and backend
 app.use(cookieParser())
-app.use(cors());
+// app.use(cors());
 
 app.use(cors({
   origin: "http://localhost:3000",
